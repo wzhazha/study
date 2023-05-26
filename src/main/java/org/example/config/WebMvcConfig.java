d1ec4de7ec75913aa7edaacf7ee5a7fc029fd2b9
zhazha <15344656790@163.com>
2023-05-26 17:37:27 +0800

    Feat: Add Web Config
    - added web config

--- src/main/java/org/example/config/WebMvcConfig.java
+++ src/main/java/org/example/config/WebMvcConfig.java
@@ -1,14 +1,24 @@
 package org.example.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
+    final Logger log = LoggerFactory.getLogger(WebMvcConfig.class);
+
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
-        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("Loading CORS configuration..........");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
     }
 }
